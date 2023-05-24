0b6f91268d24d1c47f9c4973a00daed38146b3ba
Ajmel Muadz <muadzshakir26@gmail.com>
2023-05-24 20:21:43 +0800

    completed

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -4,11 +4,15 @@ public class SortingAlgorithms
 {
     public static void main(String[] args) 
     {
+        // Methods are:
         // basicExplanation()
         // basicBubbleSort()
         // improvedBubbleSort()
+        // insertionSort()
 
-        improvedBubbleSort();
+        /* NOTE: Run ONE method in the main method unless you love getting confused.
+           This program is just to help me understand sorting algorithms. They're
+           quite fun to learn in my opinion :) */
     }
 
     // This is a basic explanation of how swapping values is made possible in Java.
@@ -53,7 +57,7 @@ public class SortingAlgorithms
         int[] list = {88, 33, 99, 22, 54};
 
         // Fully working bubble sort algorithm (ascending order)
-        int comparisonsToMake = list.length - 1;
+        int comparisonsToMake = list.length - 1;  // Improvement made
         for(int i = 0; i < list.length - 1; i++)
         {
             for(int j = 0; j < comparisonsToMake; j++)
@@ -66,8 +70,33 @@ public class SortingAlgorithms
                     list[j+1] = temp;
                 }
             }
-            comparisonsToMake--;
+            comparisonsToMake--;  // Improvement made
         }
+        System.out.println(Arrays.toString(list));
+    }
+
+    public static void insertionSort()
+    {
+        int i;
+        int j;
+        int temp;
+        int[] list = {90, 85, 65, 95, 75};
+
+        // Fully working insertion sort algorithm (ascending order)
+        i = 1;
+        while(i < list.length)
+        {
+            temp = list[i];
+            j = i - 1;
+            while(j >= 0 && list[j] > temp)  // Change '>' (second operand) to '<' for descending
+            {
+                list[j+1] = list[j];
+                j--;
+            }
+            list[j+1] = temp;
+            i++;
+        }
+
         System.out.println(Arrays.toString(list));
     }
 }
