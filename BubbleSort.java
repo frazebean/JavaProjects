1c0c4750a5ec4be30e0de71e04debef9056bf3d0
Ajmel Muadz <muadzshakir26@gmail.com>
2023-06-03 19:04:56 +0800

    completed program

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,3 +1,5 @@
+// CEO of not understanding the code I wrote.
+
 import java.util.*;
 
 public class BubbleSort
@@ -5,27 +7,29 @@ public class BubbleSort
     public static void main(String[] args) 
     {
         Scanner input = new Scanner(System.in);
-        int[] array = new int[8];
+        int[] array = new int[8], sortedArray;
         int userInput;
 
         System.out.println("Hello. Please enter 8 integers to be sorted.\n");
         for(int i = 0; i < array.length; i++)
         {
-            System.out.print("Enter array " + array[i+1] + ": ");
+            System.out.print("Enter array " + (i+1) + ": ");
             userInput = input.nextInt();
-            input.nextLine();
             array[i] = userInput;
         }
 
+        System.out.println("The array unsorted is: " + Arrays.toString(array));
+        sortedArray = bubbleSort(array);
+        System.out.println("The array sorted is: " + Arrays.toString(array));
     }
 
     public static int[] bubbleSort(int[] pArray)
     {
         int temp;
 
-        for(int pass = 0; pass < (pArray.length - 1) - 1; pass++)
+        for(int pass = 0; pass <= (pArray.length - 1) - 1; pass++)
         {
-            for(int i = 0; i < (pArray.length - 1 - pass) - 1; i++)
+            for(int i = 0; i <= (pArray.length - 1 - pass) - 1; i++)
             {
                 if(pArray[i] > pArray[i+1])
                 {
@@ -34,6 +38,7 @@ public class BubbleSort
                     pArray[i+1] = temp;
                 }
             }
+            System.out.println(Arrays.toString(pArray));
         }
 
         return pArray;
